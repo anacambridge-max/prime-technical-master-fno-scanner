92da574d6a03634c75ee343422377d982196fb7a
anacambridge-max <anacambridge@gmail.com>
2026-08-26 14:23:45 +0530

    chore: export Upstox integration primitives

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export * from "./domain/fno";
 export * from "./domain/fno-universe";
 export { primeConfig } from "./config/prime";
+export { getUpstoxConfig } from "./integrations/upstox/config";
+export {
+  buildUpstoxAuthorizationUrl,
+  exchangeAuthorizationCode,
+} from "./integrations/upstox/oauth";
+export { createOAuthState, safeEqualState } from "./integrations/upstox/state";
+export { upstoxGet } from "./integrations/upstox/client";
