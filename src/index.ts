2c91b396f1e01d01f99b4016b614a52ae3bac38e
anacambridge-max <anacambridge@gmail.com>
2026-09-12 11:44:43 +0530

    feat: export Prime engine and historical data APIs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./domain/fno";
 export * from "./domain/fno-universe";
+export * from "./domain/prime";
 export { primeConfig } from "./config/prime";
 export { getUpstoxConfig } from "./integrations/upstox/config";
 export {
@@ -8,3 +9,9 @@ export {
 } from "./integrations/upstox/oauth";
 export { createOAuthState, safeEqualState } from "./integrations/upstox/state";
 export { upstoxGet } from "./integrations/upstox/client";
+export { getHistoricalCandles } from "./integrations/upstox/historical";
+export { describeCandle } from "./engine/prime/candle";
+export { emaSeries, emaContext } from "./engine/prime/ema";
+export { averageVolume, classifyVolume } from "./engine/prime/volume";
+export { buildYesterdayLevels, nearestOpposingLevel } from "./engine/prime/levels";
+export { classifyLevelReaction } from "./engine/prime/reaction";
